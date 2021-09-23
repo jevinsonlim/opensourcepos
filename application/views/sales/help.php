ba2cae11be82341179debfe3801f8ec454f8498c
WebShells <26513147+WebShells@users.noreply.github.com>
2021-09-23 23:09:51 +0300

    Keyboard Shortcuts/Hotkeys
    Added Shortcuts/Hotkeys to Register
    
    ESC 	Cancels Current Quote/Invoice/Sale
    ALT + 1 	Item Search
    ALT + 2 	Customer Search
    ALT + 3 	Suspend Current Sale
    ALT + 4 	Show Suspended Sales
    ALT + 5 	Edit Amount Tendered
    ALT + 6 	Add Payment
    ALT + 7 	Add Payment and Complete Invoice/Sale
    ALT + 8 	Finish Quote/Invoice witdout payment
    ALT + 9 	Open Shortcuts Window

---
+++ application/views/sales/help.php
@@ -0,0 +1,50 @@
+<table class="table table-striped table-hover">
+	<thead>
+		<tr>
+			<th><?php echo $this->lang->line('sales_key_help'); ?></th>
+			<th><?php echo $this->lang->line('sales_key_function'); ?></th>
+		</tr>
+  </thead>
+	<tbody>
+		<tr>
+			<td><code>ESC</code></td>
+			<td><?php echo $this->lang->line('sales_key_cancel'); ?></td>
+		</tr>
+		<tr>
+			<td><code>ALT + 1</code></td>
+			<td><?php echo $this->lang->line('sales_key_item_search'); ?></td>
+		</tr>
+		<tr>
+			<td><code>ALT + 2</code></td>
+			<td><?php echo $this->lang->line('sales_key_customer_search'); ?></td>
+		</tr>
+		<tr>
+			<td><code>ALT + 3</code></td>
+			<td><?php echo $this->lang->line('sales_key_suspend'); ?></td>
+		</tr>
+		<tr>
+			<td><code>ALT + 4</code></td>
+			<td><?php echo $this->lang->line('sales_key_suspended'); ?></td>
+		</tr>
+		<tr>
+			<td><code>ALT + 5</code></td>
+			<td><?php echo $this->lang->line('sales_key_tendered'); ?></td>
+		</tr>
+		<tr>
+			<td><code>ALT + 6</code></td>
+			<td><?php echo $this->lang->line('sales_key_payment'); ?></td>
+		</tr>
+		<tr>
+			<td><code>ALT + 7</code></td>
+			<td><?php echo $this->lang->line('sales_key_finish_sale'); ?></td>
+		</tr>
+		<tr>
+			<td><code>ALT + 8</code></td>
+			<td><?php echo $this->lang->line('sales_key_finish_quote'); ?></td>
+		</tr>
+		<tr>
+			<td><code>ALT + 9</code></td>
+			<td><?php echo $this->lang->line('sales_key_help_modal'); ?></td>
+		</tr>
+	</tbody>
+</table>
